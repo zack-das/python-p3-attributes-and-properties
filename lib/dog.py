3856900f90f7423b5dad85fa453ae765e4a5ae92
zack-das <izackdas@gmail.com>
2025-11-04 07:11:47 +0300

    Updates

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,36 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name = "Pup", breed = "Mastiff"):
+      self.name = name
+      self.breed = breed
+
+    def get_name(self) :
+       return self._name
+
+    def get_breed(self) :
+       return self._breed
+
+
+    def data_type(self, name):
+       if((type(name) == str)and (1<= len(name) <=25)):
+          self._name = name
+       else:
+             print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, data_type)
+
+    def validity_of_breed(self,breed):
+       if breed in APPROVED_BREEDS:
+          self._breed = breed
+       else:
+          print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, validity_of_breed)
+
+
+fido = Dog(name="Fido")
+fido.data_type("Fido")
+fido.validity_of_breed("Chihuahua")
+
+
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ #!/usr/bin/env python3
 
 APPROVED_JOBS = [
     "Admin",
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "Keanu", job = "architect"):
+        self.job = job
+        self.name = name
+
+    def get_name(self):
+        return self._name
+
+    def get_job(self):
+        return self._job
+
+    def validity(self, name):
+       if((type(name) == str) and (1<= len(name) <=25)):
+          self._name = name.title()
+       else:
+          print("Name must be string between 1 and 25 characters.")
+    name = property(get_name, validity)
+
+    def approval(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+           print("Job must be in list of approved jobs." )
+    job = property(get_job, approval)
+
+
+rooney = Person(name="Rooney")
+rooney.validity("Rooney")
+rooney.approval("Plumber")
+
